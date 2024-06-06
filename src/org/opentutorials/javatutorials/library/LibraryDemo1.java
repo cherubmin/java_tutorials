2fb3e01bd7a7b0726090bc8b9b5904c754a93bb9
cherubmin <cherubjeongmin@gmail.com>
2024-06-06 20:48:08 +0900

    feat

---
+++ src/org/opentutorials/javatutorials/library/LibraryDemo1.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javatutorials.library;
+
+public class LibraryDemo1 {
+    public static void main(String[] args) {
+        /*
+        System.out.println(1);에서
+        prrintln은 메서드
+        System은 클래스
+        out은 그 클래스의 필드(변수)
+         */
+        System.out.println(1);
+        /*
+        System은 import java.lang.*;에 있다.
+        임포트 안해도 자동으로 임포트 한다.
+         */
+    }
+}
